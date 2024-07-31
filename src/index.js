0865e915abd558bc3f83bb6b5fda668c83000155
Ahmed Hamouda <ahmedabdelrazzak1001010@gmail.com>
2024-07-31 15:33:11 -0700

    Added

--- src/index.js
+++ src/index.js
@@ -1,33 +1,13 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import { CartProvider } from "./cart_context";
-
-const App = lazy(() => import("./App"));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<CartProvider>
-			<Suspense fallback={<div>Loading...</div>}>
-				<App />
-			</Suspense>
+			<App />
 		</CartProvider>
 	</React.StrictMode>
 );
-
-// Register service worker for caching static assets
-if ("serviceWorker" in navigator) {
-	window.addEventListener("load", function () {
-		navigator.serviceWorker.register("/sw.js").then(
-			function (registration) {
-				console.log(
-					"ServiceWorker registration successful with scope: ",
-					registration.scope
-				);
-			},
-			function (err) {
-				console.log("ServiceWorker registration failed: ", err);
-			}
-		);
-	});
-}
