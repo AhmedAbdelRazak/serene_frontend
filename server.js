da250108862fc43812c8776e98c9a2b296c94af6
Ahmed Hamouda <ahmedabdelrazzak1001010@gmail.com>
2024-08-02 10:58:16 -0700

    Added

--- server.js
+++ server.js
@@ -1,42 +1,19 @@
-/** @format */
-
 const express = require("express");
 const compression = require("compression");
 const path = require("path");
-const helmet = require("helmet");
 const app = express();
 
-// Use helmet for basic security enhancements
-app.use(helmet());
-
-// Enable gzip compression
 app.use(compression());
+app.use(express.static(path.join(__dirname, "build")));
 
-// Serve static assets from the "build" directory with caching
-app.use(
-	express.static(path.join(__dirname, "build"), {
-		maxAge: "1y", // Cache static assets for 1 year
-		etag: false,
-	})
-);
-
-// Serve static assets from the "public" directory with caching
-app.use(
-	express.static(path.join(__dirname, "public"), {
-		maxAge: "1y", // Cache static assets for 1 year
-		etag: false,
-	})
-);
-
-// Catch-all route to serve the React app's index.html for SPA
-app.get("*", (req, res) => {
+app.get("*", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-// Set the port from environment variables or default to 3101
+app.use(express.static(__dirname + "/public"));
+
 const PORT = process.env.PORT || 3101;
 
-// Start the server
 app.listen(PORT, () => {
 	console.log(`App is running on port ${PORT}`);
 });
--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CartProvider } from "./cart_context";
 
-// Create the root element for React
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// Render the React application
 root.render(
 	<React.StrictMode>
 		<CartProvider>
