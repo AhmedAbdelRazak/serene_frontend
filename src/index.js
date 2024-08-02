ca92bda642975a19a723821624e9b6dc8a966064
Ahmed Hamouda <ahmedabdelrazzak1001010@gmail.com>
2024-08-02 10:50:45 -0700

    Added

--- src/index.js
+++ src/index.js
@@ -14,20 +14,3 @@ root.render(
 		</CartProvider>
 	</React.StrictMode>
 );
-
-// Optional: Register a service worker for offline capabilities
-if ("serviceWorker" in navigator) {
-	window.addEventListener("load", () => {
-		navigator.serviceWorker
-			.register("/service-worker.js")
-			.then((registration) => {
-				console.log(
-					"Service Worker registered with scope:",
-					registration.scope
-				);
-			})
-			.catch((error) => {
-				console.error("Service Worker registration failed:", error);
-			});
-	});
-}
